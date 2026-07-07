bfbb22d916caf7c30cb7815b247efc83c1b29ede
hanh-3724 <nguyen.hoang.ha-c@sun-asterisk.com>
2026-07-07 15:26:44 +0700

    refactor: seperate swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('Demo Medium NestJs API')
-    .setDescription('The Medium NestJs API description')
-    .setVersion('1.0')
-    .addTag('medium_clonethetic')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
   
+  setupSwagger(app);
   await app.listen(process.env.APP_PORT ?? 3036);
 }
 bootstrap();
---
+++ src/swagger.ts
@@ -0,0 +1,14 @@
+import { INestApplication } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+export function setupSwagger(app: INestApplication){
+    const config = new DocumentBuilder()
+    .setTitle('Demo Medium NestJs API')
+    .setDescription('The Medium NestJs API description')
+    .setVersion('1.0')
+    .addTag('medium_clonethetic')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
